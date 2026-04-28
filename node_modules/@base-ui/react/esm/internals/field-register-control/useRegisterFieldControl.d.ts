5b462b35e8b6efe403871dd618165441d8822d83
Marc Angelo Tomol <tomolmarcangelo@gmail.com>
2026-04-28 22:45:23 +0800

    Delete node_modules directory

--- node_modules/@base-ui/react/esm/internals/field-register-control/useRegisterFieldControl.d.ts
+++
@@ -1,5 +0,0 @@
-import type { FieldControlRegistration } from "./useFieldControlRegistration.js";
-export interface UseRegisterFieldControlParameters extends FieldControlRegistration {
-  enabled?: boolean | undefined;
-}
-export declare function useRegisterFieldControl(controlRef: FieldControlRegistration['controlRef'], params: Omit<UseRegisterFieldControlParameters, 'controlRef'>): void;
